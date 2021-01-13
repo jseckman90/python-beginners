2df5bb4692bbe1d27ef37b49028f262a3600fa5c
Josh Seckman <jseckman90@gmail.com>
2021-01-12 23:17:25 -0500

    simple expressions

--- helloworld.py
+++ helloworld.py
@@ -5,3 +5,13 @@ print('*' * 10)
 name = input('What is your name? ')
 color = input('What is your favorite color? ')
 print(name + ' likes ' + color)
+
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2021 - int(birth_year)
+print(type(age))
+print(age)
+
+user_weight = input('Weight in lbs? ')
+lbs_to_kg = int(user_weight) * .453592
+print(lbs_to_kg)
