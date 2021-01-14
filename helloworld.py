f9890203663fbb21318771594b79c93dc1fea881
Josh Seckman <jseckman90@gmail.com>
2021-01-13 23:02:20 -0500

    formatted strings

--- helloworld.py
+++ helloworld.py
@@ -1,28 +1,42 @@
-# Print
-print('hello world')
-print('*' * 10)
+# # Print
+# print('hello world')
+# print('*' * 10)
 
-name = input('What is your name? ')
-color = input('What is your favorite color? ')
-print(name + ' likes ' + color)
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color)
 
-birth_year = input('Birth year: ')
-print(type(birth_year))
-age = 2021 - int(birth_year)
-print(type(age))
-print(age)
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(age))
+# print(age)
 
-user_weight = input('Weight in lbs? ')
-lbs_to_kg = int(user_weight) * .453592
-print(lbs_to_kg)
+# user_weight = input('Weight in lbs? ')
+# lbs_to_kg = int(user_weight) * .453592
+# print(lbs_to_kg)
 
 
 
-string = '100 Days of Code'
+# string = '100 Days of Code'
 
-print(string)
-print(string[0])
-print(string[0:4])
-print(string[-1])
-print(string[1:-1])
-print(string[:])
+# print(string)
+# print(string[0])
+# print(string[0:4])
+# print(string[-1])
+# print(string[1:-1])
+# print(string[:])
+
+
+###################
+# Formatted Strings
+###################
+
+first_name = "Josh"
+last_name = "Seckman"
+
+message = first_name + ' [' + last_name + '] is a coder'
+msg = f'{first_name} [{last_name}] is a coder'
+print(message)
+print(msg)
+
